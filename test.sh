6cd93667b4537cf41336760e6491544786157805
Chu Trong Khanh <5766617+KhanhCT@users.noreply.github.com>
2018-11-01 20:17:52 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@ cat > /etc/openhab2/things/xiaomi.things << EOF
 Bridge mihome:bridge:7c49eb17d303 "Xiaomi Gateway" [ serialNumber="7c49eb17d303", ipAddress="192.168.0.112", port=9898, key="4ED0259DBFB34424", pollingInterval=6000 ]
 {
     Thing mihome:gateway:7c49eb17d303 "Xiaomi Mi Smart Home Gateway" [itemId="7c49eb17d303"]
-
     Thing mihome:ctrl_ln2:158d00024db8f7 "Aqara Wireless Wall Switch" [itemId="158d00024db8f7"]
 }
 EOF
@@ -16,25 +15,27 @@ Number Gateway_AmbientLight <sun> { channel="mihome:gateway:7c49eb17d303:illumin
 Number Gateway_Sound <soundvolume-0> { channel="mihome:gateway:7c49eb17d303:sound" }
 Switch Gateway_SoundSwitch <soundvolume_mute> { channel="mihome:gateway:7c49eb17d303:enableSound" }
 Dimmer Gateway_SoundVolume <soundvolume> { channel="mihome:gateway:7c49eb17d303:volume" }
+Switch SW1 <light> { channel="mihome:ctrl_ln2:158d00024db8f7:ch1" }
+Switch SW2 <light> { channel="mihome:ctrl_ln2:158d00024db8f7:ch2" }
 EOF
 cat > /etc/openhab2/rules/xiaomi.rules << EOF
-rule "corLightOn"
+rule "SW1_ON"
 when   
-    Item Gateway_SoundSwitch changed from OFF to ON
+    Item SW1 changed from OFF to ON
 then   
-    sendCommand(Gateway_SoundSwitch, ON)
+    sendCommand(SW1, ON)
 end
-rule "corLightOff"
+rule "SW1_OFF"
 when   
-    Item Gateway_SoundSwitch changed from ON to OFF
+    Item SW1 changed from ON to OFF
 then   
-    sendCommand(Gateway_SoundSwitch, OFF)
+    sendCommand(SW1, OFF)
 end
 EOF
 cat > /etc/openhab2/sitemaps/xiaomi.sitemap << EOF
 sitemap xiaomi label="Xiaomi" {
 	Frame {
-        Switch item=Gateway_SoundSwitch icon="light"
+        Switch item=SW1_OFF icon="light"
     }
 }
 EOF
